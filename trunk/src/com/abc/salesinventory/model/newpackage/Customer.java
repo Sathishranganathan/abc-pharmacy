d33603c94399efa2d47ae6a441124d6796a46e6b
manuri1989@gmail.com <manuri1989@gmail.com@5ca48f59-d440-53a9-f6b7-6d67c13cf4b2>
2014-06-26 09:29:03 +0000

    Customer Class

--- trunk/src/com/abc/salesinventory/model/newpackage/Customer.java
+++ trunk/src/com/abc/salesinventory/model/newpackage/Customer.java
@@ -11,5 +11,64 @@ package com.abc.salesinventory.model.newpackage;
  * @author Manuri
  */
 public class Customer {
+    private String customerId;
+    private String customerName;
+    private String customerMobile;
+    private String customerHome;
+    private String customerOffice;
+    private String customerEmail;
     
+    public String getCustomerId()
+    {
+        return customerId;
+    }
+    public void setCustomerId(String customerId)
+    {
+        this.customerId = customerId;
+    }
+    
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+    public void setCustomerName(String customerName)
+    {
+        this.customerName = customerName;
+    }
+    
+    public String getCustomerMobile()
+    {
+        return customerMobile;
+    }
+    public void setCustomerMobile(String customerMobile)
+    {
+        this.customerMobile = customerMobile;
+    }
+    
+    public String getCustomerHome()
+    {
+        return customerHome;
+    }
+    public void setCustomerHome(String customerHome)
+    {
+        this.customerHome = customerHome;
+    }
+    
+    public String getCustomerOffice()
+    {
+        return customerOffice;
+    }
+    public void setCustomerOffice(String customerOffice)
+    {
+        this.customerOffice = customerOffice;
+    }
+    
+    public String getCustomerEmail()
+    {
+        return customerEmail;
+    }
+    public void setCustomerEmail(String customerEmail)
+    {
+        this.customerEmail = customerEmail;
+    }
 }
