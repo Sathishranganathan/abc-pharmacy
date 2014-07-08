eb4b3388707c6e2a4b3d6a851337c58d875ea964
manuri1989 <manuri1989@gmail.com>
2014-07-08 14:22:57 +0000

    Added transaction model class, transaction detail model class, stock model class, create user role UI and edit user role UI

---
+++ src/com/abc/salesinventory/model/newpackage/TransactionDetail.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.abc.salesinventory.model.newpackage;
+
+/**
+ *
+ * @author Manuri
+ */
+public class TransactionDetail {
+    private String tansactionDetailId;
+    private String transactionId;
+    private String productId;
+    private double  unitPrice;
+    private int discount;
+    private String expDate;
+    private int qty;
+
+    /**
+     * @return the tansactionDetailId
+     */
+    public String getTansactionDetailId() {
+        return tansactionDetailId;
+    }
+
+    /**
+     * @param tansactionDetailId the tansactionDetailId to set
+     */
+    public void setTansactionDetailId(String tansactionDetailId) {
+        this.tansactionDetailId = tansactionDetailId;
+    }
+
+    /**
+     * @return the transactionId
+     */
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    /**
+     * @param transactionId the transactionId to set
+     */
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    /**
+     * @return the productId
+     */
+    public String getProductId() {
+        return productId;
+    }
+
+    /**
+     * @param productId the productId to set
+     */
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * @return the unitPrice
+     */
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    /**
+     * @param unitPrice the unitPrice to set
+     */
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    /**
+     * @return the discount
+     */
+    public int getDiscount() {
+        return discount;
+    }
+
+    /**
+     * @param discount the discount to set
+     */
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    /**
+     * @return the expDate
+     */
+    public String getExpDate() {
+        return expDate;
+    }
+
+    /**
+     * @param expDate the expDate to set
+     */
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    /**
+     * @return the qty
+     */
+    public int getQty() {
+        return qty;
+    }
+
+    /**
+     * @param qty the qty to set
+     */
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    
+}
