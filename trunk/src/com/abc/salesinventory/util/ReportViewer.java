6389ddbe98c9ab37099b0c39d32d2e8791808085
ruwan.jayaweera@gmail.com <ruwan.jayaweera@gmail.com@5ca48f59-d440-53a9-f6b7-6d67c13cf4b2>
2014-07-13 06:54:22 +0000

    jasper report integration and report viewer

--- trunk/src/com/abc/salesinventory/util/ReportViewer.java
+++ trunk/src/com/abc/salesinventory/util/ReportViewer.java
@@ -2,73 +2,81 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.abc.salesinventory.util;
 
-import com.abc.salesinventory.util.Database;
 import java.awt.Container;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.*;
 import javax.swing.*;
 import java.util.HashMap;
-import java.util.Map;
-
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.swing.JRViewer;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
- * @author Sasika Suchithra
+ * @author
  */
 public class ReportViewer extends JFrame {
 
+    private String reportPath = "C:\\Users\\rdjayawe\\Downloads\\ayesha_project\\source\\SalesInventorySystem\\src\\com\\abc\\salesinventory\\report\\";
 
     public ReportViewer(String fileName) {
         this(fileName, null);
-
     }
 
-    public ReportViewer(String fileName,String s) {
+    public ReportViewer(String fileName, String s) {
         super("View Reports");
-        try{
-            Connection c=Database.con();
-            HashMap params= new HashMap();
+        try {
+            Connection con = Database.con();
+            HashMap params = new HashMap();
             params.put("Project", s);
-            //JasperReport jasperReport = JasperCompileManager.compileReport(fileName);
-            //Map<String, Object> params = new HashMap<String, Object>();
-//            JasperPrint jasperPrint = JasperFillManager.fillReport(fileName,params, c);
 
-//            JasperViewer.viewReport(jasperPrint,false);
-//            JRViewer jr = new JRViewer(jasperPrint);
-//            Container cn=getContentPane();
-//            cn.add(jr);
-            
+            InputStream stream = new FileInputStream(new File(reportPath+fileName));
+            JasperDesign jasperDesign = JRXmlLoader.load(stream);
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, con);
+
+            JasperViewer.viewReport(jasperPrint, false);
+            JRViewer jr = new JRViewer(jasperPrint);
+            Container cn = getContentPane();
+            cn.add(jr);
+
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+
+            System.out.println("ERROR: " + cnfe.getMessage());
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            System.out.println("ERROR: " + sqle.getMessage());
+        } catch (JRException jre) {
+            jre.printStackTrace();
+            System.out.println("ERROR: " + jre.getMessage());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error in generating Reports !!", "Error", 0);
+            System.out.println("ERROR: " + e.getMessage());
         }
-//        catch(ClassNotFoundException cnfe)          {
-//            //cnfe.printStackTrace();
-//
-//            System.out.println("ERROR: "+cnfe.getMessage());
-//        }
-//        catch(SQLException sqle)  {
-//        //sqle.printStackTrace();
-//            System.out.println("ERROR: "+sqle.getMessage());
-//        }
-//        catch(JRException jre) {
-//        //jre.printStackTrace();
-//            System.out.println("ERROR: "+jre.getMessage());
-//        }
 
-        catch(Exception e){
-            JOptionPane.showMessageDialog(null,"Error in generating Reports !!" , "Error", 0);
-            System.out.println("ERROR: "+e.getMessage());
-
-        }
-        
         setBounds(10, 10, 1000, 800);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationByPlatform(true);
-//        pack();
-//        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-////        java.awt.Dimension dialogSize = getSize();
-//
-//        setLocation(283,getLocation().y);
+        pack();
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+//        java.awt.Dimension dialogSize = getSize();
+
+        setLocation(283, getLocation().y);
     }
 
+    public static void main(String a[]) {
+        ReportViewer reportViewer = new ReportViewer("report1.jrxml");
+
+    }
 }
