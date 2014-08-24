04e424c6b4bd0c8a345467eb8f3865ccaacdc1b3
manuri1989@gmail.com <manuri1989@gmail.com@5ca48f59-d440-53a9-f6b7-6d67c13cf4b2>
2014-08-24 15:39:13 +0000

    permision.sql

--- trunk/db/permissions.sql
+++ trunk/db/permissions.sql
@@ -1,6 +1,15 @@
 INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('1', 'Sales', 'DO_SALES', 'Perform sales');
 
-INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('2', 'Purchases', 'DO_PURCHASES', 'Perform purchases');
+INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('2', 'Purachases', 'DO_PURCHASES', 'Perform purchases');
+
 INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('3', 'Users', 'MANAGE_USERS', 'Manage users');
 
 INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('4', 'Roles', 'MANAGE_ROLES', 'Manage roles');
+
+INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('5', 'Customers', 'MANAGE_CUSTOMERS', 'Manage customers');
+
+INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('6', 'Suppliers', 'MANAGE_SUPPLIERS', 'Manage suppliers');
+
+INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('7', 'Products', 'MANAGE_PRODUCTS', 'Manage products');
+
+INSERT INTO `pharmacy`.`permission` (`id`, `name`, `code`, `description`) VALUES ('8', 'Reports', 'GENERATE_REPORTS', 'Generate reports');
