63e5b3907b8eb7b46f8f53fa4b32d7ad88c609e9
manuri1989@gmail.com <manuri1989@gmail.com@5ca48f59-d440-53a9-f6b7-6d67c13cf4b2>
2014-08-27 17:00:10 +0000

    Daily Sales Report

--- trunk/src/com/abc/salesinventory/report/DailySalesReport.java
+++ trunk/src/com/abc/salesinventory/report/DailySalesReport.java
@@ -6,7 +6,10 @@
 
 package com.abc.salesinventory.report;
 
+import com.abc.salesinventory.model.newpackage.Transaction;
 import com.abc.salesinventory.util.DatePicker;
+import com.abc.salesinventory.util.ReportViewer;
+import java.util.HashMap;
 
 /**
  *
@@ -35,6 +38,7 @@ public class DailySalesReport extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         txtDate = new javax.swing.JTextField();
         btnViewReport = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Daily Sales Report");
@@ -44,48 +48,48 @@ public class DailySalesReport extends javax.swing.JFrame {
 
         jLabel2.setText("Date");
 
-        txtDate.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                txtDateMouseClicked(evt);
-            }
-        });
-        txtDate.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                txtDateKeyPressed(evt);
+        btnViewReport.setText("View Report");
+        btnViewReport.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnViewReportActionPerformed(evt);
             }
         });
 
-        btnViewReport.setText("View Report");
+        jLabel3.setForeground(new java.awt.Color(204, 0, 51));
+        jLabel3.setText("Eg: yyyy-mm-dd");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(56, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(139, 139, 139))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(txtDate, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(37, 37, 37)
-                        .addComponent(btnViewReport)
-                        .addGap(55, 55, 55))))
+                .addGap(56, 56, 56)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(txtDate, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel3)
+                .addGap(0, 94, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnViewReport)
+                    .addComponent(jLabel1))
+                .addGap(139, 139, 139))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
+                .addGap(36, 36, 36)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
-                    .addComponent(btnViewReport))
-                .addContainerGap(231, Short.MAX_VALUE))
+                    .addComponent(jLabel3))
+                .addGap(24, 24, 24)
+                .addComponent(btnViewReport)
+                .addContainerGap(169, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -102,15 +106,13 @@ public class DailySalesReport extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtDateMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtDateMouseClicked
-        DatePicker datePicker = new DatePicker(jPanel1);
-        txtDate.setText(datePicker.setPickedDate());
-    }//GEN-LAST:event_txtDateMouseClicked
-
-    private void txtDateKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtDateKeyPressed
-         DatePicker datePicker = new DatePicker(jPanel1);
-        txtDate.setText(datePicker.setPickedDate());
-    }//GEN-LAST:event_txtDateKeyPressed
+    private void btnViewReportActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewReportActionPerformed
+        HashMap map = new HashMap();
+       // String transactionid = ((Transaction) cmbTransaction.getSelectedItem()).getTransactionId();
+        
+       map.put("date", txtDate.getText().trim());
+        ReportViewer reportViewer = new ReportViewer("DailySalesReport.jrxml", map);
+    }//GEN-LAST:event_btnViewReportActionPerformed
 
     /**
      * @param args the command line arguments
@@ -151,6 +153,7 @@ public class DailySalesReport extends javax.swing.JFrame {
     private javax.swing.JButton btnViewReport;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txtDate;
     // End of variables declaration//GEN-END:variables
